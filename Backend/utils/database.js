47e73ebed2f1e1f9241fe52893b252a88fb0e44b
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-07-13 10:07:21 +0545

    Connected to database

--- Backend/utils/database.js
+++ Backend/utils/database.js
@@ -1,35 +1,34 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://bikal:4nFuaiDNvjINBnH7@tu-searchable-directory.amugqkd.mongodb.net/?retryWrites=true&w=majority')
-.then(client => {
-    _db = client.db();
-    if(_db){
-        console.log('connected')
-    } else {
-        console.log('not connected')
-    }
-    callback();
-}).catch(
-    err => {
-        console.log(err);
-        throw(err);
-    }
-);
-
-}
+  MongoClient.connect(
+    "mongodb+srv://077bct087sujan:NDT36ZV69hGVXBHu@cluster0.4vfghe7.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      _db = client.db();
+      if (_db) {
+        console.log("connected");
+      } else {
+        console.log("not connected");
+      }
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
 
 const getDb = () => {
-    if(_db){
-        return _db;
-    }
-    throw "No Database Found!";
-}
-
+  if (_db) {
+    return _db;
+  }
+  throw "No Database Found!";
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
