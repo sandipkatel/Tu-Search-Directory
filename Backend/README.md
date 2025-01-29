fa45d9c779fb22cc02011b2847da888fda101383
Sandip Katel <078bct077.sandip@pcampus.edu.np>
2025-01-29 23:21:37 +0545

    Delete Backend directory

--- Backend/README.md
+++
@@ -1,3 +0,0 @@
-## NODE.JS API FOR TU SEARCHABLE DIRECTORY
-
-To start the server(in development mode), navigate to the backend directory and type in the command npm start.
--- Backend/routes/retrieveRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const retrieveHandler = require('../handlers/retrieveHandler');
-
-router.get('/nodes', retrieveHandler);
-
-module.exports = router;
