6f6a3beef54889d7c4f32ac220ca3e667a8e72d7
Sandip Katel <078bct077.sandip@pcampus.edu.np>
2025-01-29 23:22:11 +0545

    Delete Frontend/TU-Searchable-Directory directory

--- Frontend/TU-Searchable-Directory/src/app/models/node.model.ts
+++
@@ -1,5 +0,0 @@
-export class Node{
-    label: string;
-    data: {root: boolean, _id: string};
-    children: Node[];   
-}
--- Frontend/TU-Searchable-Directory/src/app/models/organization.model.ts
+++
@@ -1,7 +0,0 @@
-export class Organization{
-    _id: string;
-    name: string;
-    info: {personnel: {name: string, title: string, imageUrl: string}[], programmes: {name: string, description: string}[]};
-    children: string[];
-    admin: boolean   
-}
