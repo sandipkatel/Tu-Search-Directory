dd83ab295129eae73ef17a4ea307bb0ede31d069
sandipkatel <babusandipkatel@gmail.com>
2025-02-24 09:10:20 +0545

    create additional pages

---
+++ Frontend/src/app/Centeral-Offices/page.js
@@ -0,0 +1,5 @@
+import React, { UseState } from "react";
+
+export default function Faculties() {
+  return <div>This is Centeral Office's Page</div>;
+}
---
+++ Frontend/src/app/Faculties/page.js
@@ -0,0 +1,5 @@
+import React, { UseState } from "react";
+
+export default function Faculties() {
+  return <div>This is Facultie's Page</div>;
+}
