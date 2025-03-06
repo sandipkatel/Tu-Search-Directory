564ea2beb7e0b80b49636373c0d41aa63ed63541
SijanJ <sijan2joshi@gmail.com>
2025-03-06 06:25:01 +0545

    adjusted filter position

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,22 +1,22 @@
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "8910",
-//   host: "localhost",
-//   port: "5432",
-//   database: "tu_search_directory",
-// });
-
-// module.exports = pool;
-
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "1234",
-  host: "2403:3800:3204:16bc:ecf5:70b:9ba5:b9d6",
+  password: "sijan123",
+  host: "localhost",
   port: "5432",
   database: "tu_search_directory",
-  ssl: false,
 });
 
 module.exports = pool;
+
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "1234",
+//   host: "2403:3800:3204:16bc:ecf5:70b:9ba5:b9d6",
+//   port: "5432",
+//   database: "tu_search_directory",
+//   ssl: false,
+// });
+
+// module.exports = pool;
