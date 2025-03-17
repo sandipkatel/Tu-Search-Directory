a7bcfc9ca7c061cf3aae81facb48dce7d290dd52
sandipkatel <babusandipkatel@gmail.com>
2025-03-17 18:55:01 +0545

    Base URL error in loginPage fix

--- Frontend/src/app/login/page.js
+++ Frontend/src/app/login/page.js
@@ -58,7 +58,7 @@ const SignInForm = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/auth/login", {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ Email, Password }),
